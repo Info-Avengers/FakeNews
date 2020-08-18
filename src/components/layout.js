222a0eef8bc711e5fb3e3d5c42d0f829b21641f0
Ross Ragsdale <ross.ragsdale@gmail.com>
2020-08-18 12:07:16 -0500

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -59,9 +59,7 @@ const Layout = ({ location, title, children }) => {
       <header>{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+        Securely built in {new Date().getFullYear()}
       </footer>
     </div>
   )
