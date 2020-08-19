59be1499e22659c17a70558bcab7b074c2890c14
Ross Ragsdale <ross.ragsdale@gmail.com>
2020-08-18 21:24:06 -0500

    Updates.

--- amplify/backend/function/fakenews1d3c0567/src/index.js
+++ amplify/backend/function/fakenews1d3c0567/src/index.js
@@ -1,9 +1,21 @@
-const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./app');
+const AWS = require("aws-sdk");
+const awsServerlessExpress = require("aws-serverless-express");
+const app = require("./app");
 
+const sns = new AWS.SNS();
 const server = awsServerlessExpress.createServer(app);
 
 exports.handler = (event, context) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
-  return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
+  var params = {
+    Message: `A fake email report has been submitted:\n
+      ${JSON.stringify(event.body)}` /* required */,
+    TopicArn: "arn:aws:sns:us-east-1:144504656881:FakeNewsEmails",
+  };
+  sns.publish(params, function (err, data) {
+    if (err) console.log(err, err.stack);
+    // an error occurred
+    else console.log(data); // successful response
+  });
+  return awsServerlessExpress.proxy(server, event, context, "PROMISE").promise;
 };
